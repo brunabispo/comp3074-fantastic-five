7c798bd394ecf4748db57c021760b694ab86e9bb
Bruna Abatepaulo <bruna.abatepaulo@hotmail.com>
2020-11-20 16:41:50 -0300

    register page

---
+++ app/src/main/java/ca/gbc/comp3074/mind_manager_app/RegisterActivity.java
@@ -0,0 +1,18 @@
+package ca.gbc.comp3074.mind_manager_app;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView lblConfirmPass = findViewById(R.id.lblConfirmPass);
+        lblConfirmPass.setText("Confirm\nPassword");
+    }
+}
